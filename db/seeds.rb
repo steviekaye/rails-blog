b59edd2c9ec5227d4139068f990fd8722b8cd836
steviekaye <stevie.kaye@gmail.com>
2018-10-04 18:39:36 +1300

    Add everything up to 9.1 Basic Authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  Article.create(title: "Blog ##{i}", text: "This is some example text.")
+end
