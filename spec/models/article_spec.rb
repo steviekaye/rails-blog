183d3be31200ae35d66d4c4f2403ce713de214a9
steviekaye <stevie.kaye@gmail.com>
2018-11-02 16:28:27 +1300

    Add model testing

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+
+  context "when a title has a length of at least five characters" do
+    it "is valid" do
+      article = Article.new(
+        title: "Title",
+        category: Category.new(),
+        text: "I'm an article"
+      )
+      expect(article).to be_valid
+    end
+  end
+
+  context "when a title has a length of less than five characters" do
+    it "is invalid" do
+      article = Article.new(
+        title: "Wow",
+        category: Category.new(),
+        text: "I'm an article"
+      )
+      article.valid?
+      expect(article.errors[:title]).to include("is too short (minimum is 5 characters)")
+    end
+  end
+
+  context "when the body has a length of at least two characters" do
+    it "is valid" do
+      article = Article.new(
+        title: "Title",
+        category: Category.new(),
+        text: "I'm an article"
+      )
+      expect(article).to be_valid
+    end
+  end
+
+  context "when the body has a length of less than two characters" do
+    it "is invalid" do
+      article = Article.new(
+        title: "Wow",
+        category: Category.new(),
+        text: "a"
+      )
+      article.valid?
+      expect(article.errors[:text]).to include("is too short (minimum is 2 characters)")
+    end
+  end
+
+
+end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+
+  context "when a commenter exists" do
+    it "is valid" do
+      comment = Comment.new(
+        commenter: "Mr T",
+        article: Article.new(),
+        body: "I'm a comment"
+      )
+      expect(comment).to be_valid
+    end
+  end
+
+  context "when a commenter doesn't exist" do
+    it "is invalid" do
+      comment = Comment.new(
+        article: Article.new(),
+        body: "I'm a comment"
+      )
+      comment.valid?
+      expect(comment.errors[:commenter]).to include("can't be blank")
+    end
+  end
+
+  context "when a comment body doesn't exist" do
+    it "is invalid" do
+      comment = Comment.new(
+        commenter: "Mr T",
+        article: Article.new()
+      )
+      comment.valid?
+      expect(comment.errors[:body]).to include("can't be blank")
+    end
+  end
+
+  context "when an associated article doesn't exist" do
+    it "is invalid" do
+      comment = Comment.new(
+        commenter: "Mr T",
+        body: "I'm a comment"
+      )
+      comment.valid?
+      expect(comment.errors[:article]).to include("must exist")
+    end
+  end
+
+end
