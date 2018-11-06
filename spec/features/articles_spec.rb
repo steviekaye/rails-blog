ec003ced2876d48dfada22d36d6dca3f3a79f08e
steviekaye <stevie.kaye@gmail.com>
2018-11-06 18:21:33 +1300

    Added feature tests

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -5,6 +5,22 @@ RSpec.feature "Articles", type: :feature do
     @category = FactoryBot.create(:category)
   end
 
+  scenario "unsucessfully create a new article" do
+    article = FactoryBot.build(:article)
+
+    visit root_path
+    click_link "New article"
+
+    expect {
+      fill_in "Title", with: article.title
+      select 'My Awesome Category', from: 'category_id'
+      click_on "Create Article"
+    }.to change(Article.all, :count).by(0)
+
+    expect(page).to have_content "Text can't be blank"
+    expect(page).to have_content "Text is too short"
+  end
+
   scenario "creates a new article" do
     article = FactoryBot.build(:article)
 
@@ -22,4 +38,37 @@ RSpec.feature "Articles", type: :feature do
     expect(page).to have_content @category.name
     expect(page).to have_content article.text
   end
+
+  scenario "edit an article" do
+    article = FactoryBot.create(:article)
+
+    visit root_path
+    click_link 'Edit article'
+
+    expect(page).to have_content "Edit article"
+
+    expect {
+      fill_in "Text", with: "This has been edited"
+      click_on "Update Article"
+    }.to change(Article.all, :count).by(0)
+
+    expect(page).to have_content "This has been edited"
+  end
+
+  # commenting out deletion scenario while geckodriver/selenium/firefox
+  # interactions are giving     Selenium::WebDriver::Error::UnknownError:
+  #     invalid argument: can't kill an exited proces
+
+  # scenario "delete an article", js: true do
+  #   article = FactoryBot.create(:article)
+  #
+  #   visit root_path
+  #
+  #   expect {
+  #   click_link 'Delete article'
+  #   page.accept_alert 'Are you sure?' do
+  #     click_button('OK')
+  #   end
+  #   }.to change(Article.all, :count).by(-1)
+  # end
 end
---
+++ spec/features/comments_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "Comments", type: :feature do
+
+  scenario "create a comment" do
+    comment = FactoryBot.build(:comment)
+
+    visit root_path
+    click_link 'Read article'
+
+    expect {
+      fill_in "Commenter", with: comment.commenter
+      fill_in "Body", with: comment.body
+      click_on "Create Comment"
+    }.to change(Comment.all, :count).by(1)
+
+    expect(page).to have_content comment.commenter
+    expect(page).to have_content comment.body
+  end
+end
